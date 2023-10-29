6210dbf978196c7369d33adcd6d75008cdc6519b
sehoon0302 <saehun66@gmail.com>
2023-10-29 13:14:03 +0900

    TextView에서 실제 데이터 연동

--- app/src/main/java/kr/ac/wku/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/adapters/StudentAdapter.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.ac.wku.listview_20231029.R
 import kr.ac.wku.listview_20231029.datas.StudentData
 
 class StudentAdapter(
     mContext: Context,
     resId: Int,
-    mList: ArrayList<StudentData>
+    val mList: ArrayList<StudentData>   //getView "함수"에서도 사용하고 싶다 : val 키워드 추가 -> 멤버변수로 등록
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) { //ArrayAdapter: //기본생성자 지원 X
 
     //커스텀으로 만든 xml을 -> 코틀린단으로 끌어와주는 객체
@@ -28,6 +29,22 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+        //xml을 그려낸 row 객체 내부에서, 텍스트뷰 등을 찾아서 실제 데이터로 연동
+        //실제 데이터? 어떤 학생 => mList의 position에 맞는 객체
+
+        val stdData = mList[position]   //그림을 그려줄 위치에 맞는 학생객체 추출
+
+        //row안의 이름 표시하는 TextView를 변수로 가져와야, stdData의 이름 값을 대입 가능
+        //Adapter에서는 데이터 바인딩 사용 불가 -> 직접 코드로 불러내야 함 (텍스트뷰들 가져오기)
+
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtPhoneNum = row.findViewById<TextView>(R.id.txtPhoneNum)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+
+        txtName.text = stdData.name
+        txtPhoneNum.text = stdData.phoneNum
+        txtAge.text = "${2023 - stdData.birthYear + 1}세"
 
         return row
     }
--- app/src/main/java/kr/ac/wku/listview_20231029/datas/StudentData.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/datas/StudentData.kt
@@ -1,6 +1,6 @@
 package kr.ac.wku.listview_20231029.datas
 
-class StudentData(name: String, birthYear: Int, phoneNum: String) {
+class StudentData( val name: String, val birthYear: Int, val phoneNum: String) {
     //StudentData의 생성자 변경 : 이름, 출생년도, 폰번호를 넣어서 생성하도록
     //
 }

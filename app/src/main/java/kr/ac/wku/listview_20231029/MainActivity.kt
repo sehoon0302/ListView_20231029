c9884db9bc15ef144592b6146d34f1a391fe4bc4
sehoon0302 <saehun66@gmail.com>
2023-10-29 11:50:55 +0900

    리스트뷰에 항목 수 만큼 리스트 표시

--- app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
@@ -2,10 +2,34 @@ package kr.ac.wku.listview_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.listview_20231029.adapters.StudentAdapter
+import kr.ac.wku.listview_20231029.databinding.ActivityMainBinding
+import kr.ac.wku.listview_20231029.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+
+    lateinit var mStudentAdapter: StudentAdapter
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //화면이 켜질때 학생목록을 ArrayList에 수기로 추가(임시활용)
+        mStudentList.add(StudentData("김김김",1983,"010-1234-5678"))
+        mStudentList.add(StudentData("김정은",1999,"010-2345-6789"))
+        mStudentList.add(StudentData("바이든",2002,"010-3456-7890"))
+        mStudentList.add(StudentData("김학생",2000,"010-4567-8901"))
+        mStudentList.add(StudentData("대학생",1997,"010-5678-9012"))
+        mStudentList.add(StudentData("머학생",1994,"010-6789-0123"))
+
+        //어탭터 변수도 객체 생성
+        mStudentAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList )
+
+        binding.studentListView.adapter = mStudentAdapter
     }
 }
--- app/src/main/java/kr/ac/wku/listview_20231029/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package kr.ac.wku.listview_20231029.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.ac.wku.listview_20231029.R
 import kr.ac.wku.listview_20231029.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,22 @@ class StudentAdapter(
     resId: Int,
     mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) { //ArrayAdapter: //기본생성자 지원 X
+
+    //커스텀으로 만든 xml을 -> 코틀린단으로 끌어와주는 객체
+
+    val inf = LayoutInflater.from(mContext)
+
+    //완성한 inf를 이용해서 -> xml을 가져와주는 함수
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
